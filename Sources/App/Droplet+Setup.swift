f7adb02c445d6abdbc662898ac6f0cc00ed4ce41
Valtteri Koskivuori <vkoskiv@gmail.com>
2017-06-18 18:50:36 +0300

    Add API description

--- Sources/App/Droplet+Setup.swift
+++ Sources/App/Droplet+Setup.swift
@@ -2,6 +2,17 @@
 
 let canvas = Canvas()
 
+
+/*
+TODO:
+
+InitialAuth (First ever join, no params?) Return UUID, then client requests getUserStats, if IP banned, return nothing
+getCanvas (Logging in, no params?) Return canvas in an efficient format
+postTile (Client posts a new tile, params: userID, X+Y coords, colorID) Return OK or incorrect param
+getTileData (User has highlighted a tile, client sends this to get the info for that tile, params: userID, X+Y coords)
+getUserStats (client has UUID, is returning, parameters: UUID) returns player stats
+*/
+
 extension Droplet {
     public func setup() throws {
 		//Set up routes
