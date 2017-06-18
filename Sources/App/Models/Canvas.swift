76311bc2ff9b1dfcb8cea431c6277163d59d7dae
Valtteri Koskivuori <vkoskiv@gmail.com>
2017-06-18 21:25:45 +0300

    Fix crash

--- Sources/App/Models/Canvas.swift
+++ Sources/App/Models/Canvas.swift
@@ -17,13 +17,14 @@ class Canvas {
 		//We've received a tile change, it's been approved, send that update to ALL connected clients
 	}
 	
+	//FIXME: Consider user.sendJSON(somejson)
 	//Send to a specific user
 	func sendJSON(to: User, json: JSON) {
 		for (uuid, socket) in connections {
 			guard uuid != to.uuid else{
 				continue
 			}
-			try? socket.send(json.string!)
+			try? socket.send(json.serialize().makeString())
 		}
 	}
 	
